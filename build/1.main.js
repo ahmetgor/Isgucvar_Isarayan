11015bf6dbfe8974d92898ac536b7c432daecf36
ahmetgor <ahmet.gor@i2isystems.local>
2017-11-03 18:38:02 +0300

    linked share kaldırıldı

--- build/1.main.js
+++ build/1.main.js
@@ -149,32 +149,33 @@ var OzgecmisPage = /** @class */ (function () {
         }
     };
     OzgecmisPage.prototype.shareFace = function () {
-        var options = {
-            method: "share",
-            href: window.location.origin + '/#/detay/' + this.ozgecmis._id,
-            caption: "Such caption, very feed.",
-            description: "Much description"
-            // picture: this.ilan.resim
-        };
+        //     let options = 	{
+        //   method: "share",
+        // 	href: window.location.origin+'/#/detay/'+this.ozgecmis._id,
+        // 	caption: "Such caption, very feed.",
+        // 	description: "Much description"
+        // 	// picture: this.ilan.resim
+        // }
         console.log("share face");
         FB.ui({
             method: 'share',
-            href: window.location.origin + '/#/detay/' + this.ozgecmis._id,
+            href: 'https://isgucvarisveren.herokuapp.com' + '/#/ilandetay/' + this.ozgecmis._id,
         }, function (response) { });
     };
-    OzgecmisPage.prototype.shareLinked = function () {
-        var payload = {
-            "comment": "Yeni bir İşgüçvar ilanı!" + window.location.origin + '/#/detay/' + this.ozgecmis._id,
-            "visibility": {
-                "code": "anyone"
-            }
-        };
-        IN.API.Raw("/people/~/shares?format=json")
-            .method("POST")
-            .body(JSON.stringify(payload))
-            .result(function (onSuccess) { })
-            .error(function (onError) { });
-    };
+    //   shareLinked(){
+    // var payload = {
+    //   "comment": "Yeni bir İşgüçvar ilanı!" + window.location.origin+'/#/detay/'+this.ozgecmis._id,
+    //   "visibility": {
+    //     "code": "anyone"
+    //   }
+    // };
+    //
+    // IN.API.Raw("/people/~/shares?format=json")
+    //   .method("POST")
+    //   .body(JSON.stringify(payload))
+    //   .result((onSuccess) =>{})
+    //   .error((onError) =>{});
+    //   }
     OzgecmisPage.prototype.presentAlert = function () {
         var alert = this.alertCtrl.create({
             title: 'Özgeçmişiniz Eksik!',
@@ -194,14 +195,15 @@ var OzgecmisPage = /** @class */ (function () {
                     handler: function () {
                         _this.shareFace();
                     }
-                }, {
-                    text: 'LinkedIn',
-                    icon: 'logo-linkedin',
-                    handler: function () {
-                        console.log('Archive clicked');
-                        _this.shareLinked();
-                    }
-                }, {
+                },
+                // {
+                // text: 'LinkedIn',
+                // icon: 'logo-linkedin',
+                // handler: () => {
+                //   console.log('Archive clicked');
+                //   this.shareLinked();
+                // }},
+                {
                     text: 'İptal',
                     role: 'cancel',
                     icon: 'close',
@@ -219,7 +221,7 @@ var OzgecmisPage = /** @class */ (function () {
                 message: "share this\n",
                 // subject: 'the subject', // fi. for email
                 // files: [this.ilan.resim], // an array of filenames either locally or remotely
-                url: window.location.origin + "/#/ilan/" + this.ozgecmis._id,
+                url: 'https://isgucvarisveren.herokuapp.com' + '/#/ilandetay/' + this.ozgecmis._id,
                 chooserTitle: 'Uygulama seçin:' // Android only, you can override the default share sheet title
             };
             // this.socialSharing.shareViaFacebookWithPasteMessageHint('Message via Facebook', null, "https://isgucvar.herokuapp.com/", "paste it")
@@ -243,13 +245,10 @@ var OzgecmisPage = /** @class */ (function () {
         __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_7" /* Component */])({
             selector: 'page-ozgecmis',template:/*ion-inline-start:"C:\Users\7448\Desktop\isBul\src\pages\ozgecmis\ozgecmis.html"*/'<!--\n\n  Generated template for the Ozgecmis page.\n\n\n\n  See http://ionicframework.com/docs/v2/components/#navigation for more info on\n\n  Ionic pages and navigation.\n\n-->\n\n<ion-header>\n\n\n\n  <ion-navbar>\n\n    <button ion-button menuToggle>\n\n      <ion-icon name="menu"></ion-icon>\n\n    </button>\n\n    <ion-buttons end>\n\n  <button ion-button icon-only (click)="share()">\n\n    <ion-icon name="share"></ion-icon>\n\n  </button>\n\n</ion-buttons>\n\n    <ion-title>Özgeçmişim</ion-title>\n\n  </ion-navbar>\n\n\n\n</ion-header>\n\n\n\n<ion-content>\n\n\n\n  <div *ngIf="ozgecmis">\n\n\n\n  <ion-card class="item">\n\n    <ion-item text-wrap class="item">\n\n    <ion-thumbnail item-left>\n\n      <img class="profile-image1" [src]="ozgecmis.resim">\n\n    </ion-thumbnail>\n\n\n\n    <h1>{{ozgecmis.isim}}</h1>\n\n    <p>{{ozgecmis.unvan}}</p>\n\n    <p>{{getAge()}} yaşında, {{ozgecmis.yilTecrube}} yıl tecrübe</p>\n\n    <!-- <ion-label *ngIf="ozgecmis.enabled" color="primary" >Aktif</ion-label>\n\n    <ion-label *ngIf="!ozgecmis.enabled" color="primary" >Pasif</ion-label> -->\n\n    <!-- <ion-toggle [(ngModel)]="ozgecmis.enabled"></ion-toggle> -->\n\n  </ion-item>\n\n    <ion-item no-margin no-padding>\n\n    <ion-label class="alignme" *ngIf="ozgecmis.enabled" color="primary" >Aktif</ion-label>\n\n    <ion-label class="alignme" *ngIf="!ozgecmis.enabled" color="primary" >Pasif</ion-label>\n\n    <ion-toggle [(ngModel)]="ozgecmis.enabled" checked="ozgecmis.enabled" (ionChange)="checkOzgecmis($event)"></ion-toggle>\n\n  </ion-item>\n\n</ion-card>\n\n\n\n<div [class.ozgecmis]="!ozgecmis.enabled">\n\n\n\n<ion-card>\n\n  <ion-card-header>\n\n    <ion-row>\n\n      <ion-col>\n\n  <b>Kişisel Bilgiler</b>\n\n</ion-col>\n\n<ion-col>\n\n  <ion-buttons end>\n\n    <button ion-button small  icon-left clear (click)="itemTapped($event, ozgecmis, \'kisisel\', \'\')">\n\n    <ion-icon name="create"></ion-icon>\n\n    Düzenle\n\n  </button>\n\n</ion-buttons>\n\n</ion-col>\n\n  </ion-row>\n\n</ion-card-header>\n\n\n\n  <ion-item text-wrap>\n\n  <ion-grid no-padding>\n\n  <ion-row><ion-col col-5><h4>Doğum Tarihi:</h4></ion-col> <ion-col><h4>{{ozgecmis.dogumTarihi | date:\'dd/MM/yyyy\'}}</h4></ion-col> </ion-row>\n\n  <ion-row><ion-col col-5><h4>TC Vatandaşı: </h4></ion-col> <ion-col><h4>{{ozgecmis.tc}}</h4></ion-col> </ion-row>\n\n  <ion-row><ion-col col-5><h4>Askerlik: </h4></ion-col> <ion-col><h4> {{ozgecmis.askerlik}}</h4></ion-col> </ion-row>\n\n  <ion-row><ion-col col-5><h4>Medeni Hal:</h4> </ion-col> <ion-col><h4> {{ozgecmis.medeni}}</h4></ion-col> </ion-row>\n\n  <ion-row><ion-col col-5><h4>Ehliyet Cinsi: </h4></ion-col> <ion-col><h4> {{ozgecmis.ehliyet}}</h4></ion-col> </ion-row>\n\n  <ion-row><ion-col col-5><h4>Eğitim: </h4></ion-col> <ion-col><h4> {{ozgecmis.egitimdurum | datePipe:\'egitimdurum\'}}</h4></ion-col> </ion-row>\n\n  <!-- <ion-row><ion-col col-5><h4>Tecrübe: </h4></ion-col> <ion-col><h4> {{ozgecmis.tecrubedurum}}</h4></ion-col> </ion-row> -->\n\n</ion-grid>\n\n</ion-item>\n\n\n\n  </ion-card>\n\n\n\n<ion-card>\n\n  <ion-card-header>\n\n    <ion-row>\n\n      <ion-col>\n\n  <b>İletişim Bilgileri</b>\n\n</ion-col>\n\n<ion-col>\n\n  <ion-buttons end>\n\n    <button ion-button small  icon-left clear (click)="itemTapped($event, ozgecmis, \'iletisim\', \'\')">\n\n    <ion-icon name="create"></ion-icon>\n\n    Düzenle\n\n  </button>\n\n</ion-buttons>\n\n</ion-col>\n\n  </ion-row>\n\n</ion-card-header>\n\n  <ion-item text-wrap>\n\n    <ion-grid no-padding>\n\n  <ion-row><ion-col col-5><h4>Telefon:</h4></ion-col> <ion-col><h4>{{ozgecmis.telefon}}</h4></ion-col> </ion-row>\n\n  <ion-row><ion-col col-5><h4>Email:</h4></ion-col> <ion-col><h4>{{ozgecmis.email}}</h4></ion-col> </ion-row>\n\n  <ion-row><ion-col col-5><h4>Adres:</h4></ion-col> <ion-col><h4>{{ozgecmis.adres}}</h4></ion-col> </ion-row>\n\n</ion-grid>\n\n</ion-item>\n\n</ion-card>\n\n\n\n<ion-card>\n\n  <ion-card-header>\n\n    <ion-row>\n\n      <ion-col>\n\n  <b>Tecrübe</b>\n\n</ion-col>\n\n<ion-col>\n\n  <!-- <span ion-text style="float:right;" color="primary"> -->\n\n  <ion-buttons end>\n\n    <button ion-button small  icon-left clear (click)="itemTapped($event, {}, \'tecrubeEkle\', ozgecmis.tecrube)">\n\n    <ion-icon name="create"></ion-icon>\n\n    Ekle\n\n  </button>\n\n</ion-buttons>\n\n</ion-col>\n\n  </ion-row>\n\n  <!-- </span> -->\n\n</ion-card-header>\n\n<button ion-item *ngFor="let tecrube of (ozgecmis.tecrube | datePipe)" (click)="itemTapped($event, tecrube, \'tecrube\', ozgecmis.tecrube)">\n\n<h4><b>{{tecrube.firma}}</b> <span ion-text style="float:right;">{{tecrube.sehir}}</span></h4>\n\n<h4>{{tecrube.pozisyon}}</h4>\n\n<h4 style="white-space: pre-wrap;">{{tecrube.isTanimiKisa}}</h4>\n\n<h4>{{tecrube.giris | date:\'MM/yyyy\'}} - {{tecrube.cikis | date:\'MM/yyyy\'}}</h4>\n\n</button>\n\n</ion-card>\n\n\n\n<ion-card>\n\n  <ion-card-header>\n\n    <ion-row>\n\n      <ion-col>\n\n  <b>Eğitim</b>\n\n</ion-col>\n\n<ion-col>\n\n  <ion-buttons end>\n\n    <button ion-button small  icon-left clear (click)="itemTapped($event, {}, \'egitimEkle\', ozgecmis.egitim)">\n\n    <ion-icon name="create"></ion-icon>\n\n    Ekle\n\n  </button>\n\n</ion-buttons>\n\n</ion-col>\n\n  </ion-row>\n\n</ion-card-header>\n\n<button ion-item *ngFor="let egitim of (ozgecmis.egitim | datePipe)" (click)="itemTapped($event, egitim, \'egitim\', ozgecmis.egitim)">\n\n<h4><b>{{egitim.okul}}</b> <span ion-text style="float:right;">{{egitim.sehir}}</span></h4>\n\n<h4>{{egitim.bolum}}</h4>\n\n<h4>{{egitim.cikis | date:\'MM/yyyy\'}} - {{egitim.derece | datePipe:\'egitimdurum\'}}</h4>\n\n</button>\n\n</ion-card>\n\n\n\n<ion-card>\n\n  <ion-card-header no-margin>\n\n    <ion-row>\n\n      <ion-col>\n\n  <b>Yabancı Dil</b>\n\n</ion-col>\n\n<ion-col>\n\n  <ion-buttons end>\n\n    <button ion-button small  icon-left clear (click)="itemTapped($event, {}, \'yabanciDilEkle\', ozgecmis.yabanciDil)">\n\n    <ion-icon name="create"></ion-icon>\n\n    Ekle\n\n  </button>\n\n</ion-buttons>\n\n</ion-col>\n\n  </ion-row>\n\n</ion-card-header>\n\n<button no-margin ion-item *ngFor="let yabanciDil of ozgecmis.yabanciDil" (click)="itemTapped($event, yabanciDil, \'yabanciDil\', ozgecmis.yabanciDil)">\n\n  <h4><b>{{yabanciDil.dil}} : </b>{{yabanciDil.seviye | datePipe:\'seviye\'}}</h4>\n\n  <!-- <ion-row><ion-col col-5><h4>{{yabanciDil.dil}} : </h4></ion-col> <ion-col><h4>{{yabanciDil.seviye}}</h4></ion-col> </ion-row> -->\n\n\n\n</button>\n\n</ion-card>\n\n\n\n<ion-card>\n\n  <ion-card-header no-margin>\n\n    <ion-row>\n\n      <ion-col>\n\n  <b>Sertifikalar</b>\n\n</ion-col>\n\n<ion-col>\n\n  <ion-buttons end>\n\n    <button ion-button small  icon-left clear (click)="itemTapped($event, {}, \'sertifikaEkle\', ozgecmis.sertifika)">\n\n    <ion-icon name="create"></ion-icon>\n\n    Ekle\n\n  </button>\n\n</ion-buttons>\n\n</ion-col>\n\n  </ion-row>\n\n</ion-card-header>\n\n<button ion-item *ngFor="let sertifika of (ozgecmis.sertifika | datePipe)" (click)="itemTapped($event, sertifika, \'sertifika\', ozgecmis.sertifika)">\n\n  <h4><b>{{sertifika.ad}}</b></h4>\n\n  <h4>{{sertifika.kurum}} - {{sertifika.cikis | date:\'MM/yyyy\'}}</h4>\n\n</button>\n\n</ion-card>\n\n\n\n<ion-card>\n\n  <ion-card-header no-margin>\n\n    <ion-row>\n\n      <ion-col>\n\n  <b>Bilgisayar Teknolojileri</b>\n\n</ion-col>\n\n<ion-col>\n\n  <ion-buttons end>\n\n    <button no-margin ion-button small  icon-left clear (click)="itemTapped($event, ozgecmis, \'bilgisayar\', \'\')">\n\n    <ion-icon name="create"></ion-icon>\n\n    Düzenle\n\n  </button>\n\n</ion-buttons>\n\n</ion-col>\n\n  </ion-row>\n\n</ion-card-header>\n\n  <!-- <button ion-button small  icon-left clear (click)="itemTapped($event, ozgecmis, \'bilgisayar\')"> -->\n\n  <ion-item no-margin text-wrap>\n\n  <h4>{{ozgecmis.bilgisayar}}</h4>\n\n</ion-item>\n\n<!-- </button> -->\n\n</ion-card>\n\n\n\n</div>\n\n\n\n</div>\n\n</ion-content>\n\n'/*ion-inline-end:"C:\Users\7448\Desktop\isBul\src\pages\ozgecmis\ozgecmis.html"*/
         }),
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavParams */],
-            __WEBPACK_IMPORTED_MODULE_2__providers_ilan_ser__["a" /* IlanSer */], __WEBPACK_IMPORTED_MODULE_3__providers_ozgecmis_ser__["a" /* OzgecmisSer */], __WEBPACK_IMPORTED_MODULE_4__providers_user_auth__["a" /* UserAuth */],
-            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* AlertController */], __WEBPACK_IMPORTED_MODULE_5__ionic_storage__["b" /* Storage */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["i" /* Events */],
-            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["j" /* ActionSheetController */], __WEBPACK_IMPORTED_MODULE_8__ionic_native_facebook__["a" /* Facebook */],
-            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["k" /* Platform */], __WEBPACK_IMPORTED_MODULE_9__ionic_native_social_sharing__["a" /* SocialSharing */]])
+        __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavController */]) === "function" && _a || Object, typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavParams */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavParams */]) === "function" && _b || Object, typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_2__providers_ilan_ser__["a" /* IlanSer */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2__providers_ilan_ser__["a" /* IlanSer */]) === "function" && _c || Object, typeof (_d = typeof __WEBPACK_IMPORTED_MODULE_3__providers_ozgecmis_ser__["a" /* OzgecmisSer */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_3__providers_ozgecmis_ser__["a" /* OzgecmisSer */]) === "function" && _d || Object, typeof (_e = typeof __WEBPACK_IMPORTED_MODULE_4__providers_user_auth__["a" /* UserAuth */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_4__providers_user_auth__["a" /* UserAuth */]) === "function" && _e || Object, typeof (_f = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* AlertController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* AlertController */]) === "function" && _f || Object, typeof (_g = typeof __WEBPACK_IMPORTED_MODULE_5__ionic_storage__["b" /* Storage */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_5__ionic_storage__["b" /* Storage */]) === "function" && _g || Object, typeof (_h = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["i" /* Events */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["i" /* Events */]) === "function" && _h || Object, typeof (_j = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["j" /* ActionSheetController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["j" /* ActionSheetController */]) === "function" && _j || Object, typeof (_k = typeof __WEBPACK_IMPORTED_MODULE_8__ionic_native_facebook__["a" /* Facebook */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_8__ionic_native_facebook__["a" /* Facebook */]) === "function" && _k || Object, typeof (_l = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["k" /* Platform */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["k" /* Platform */]) === "function" && _l || Object, typeof (_m = typeof __WEBPACK_IMPORTED_MODULE_9__ionic_native_social_sharing__["a" /* SocialSharing */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_9__ionic_native_social_sharing__["a" /* SocialSharing */]) === "function" && _m || Object])
     ], OzgecmisPage);
     return OzgecmisPage;
+    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m;
 }());
 
 //# sourceMappingURL=ozgecmis.js.map
